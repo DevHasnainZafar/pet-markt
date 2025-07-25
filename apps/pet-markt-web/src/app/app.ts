fc30d8efb7c656b8d181cf070a1e33111574d425
Hasnain Zafar <hasnainpak3@gmail.com>
2025-07-26 01:34:06 +0500

    feat(daisyui): add daisyui to project setup daisyui and config it in tailwindconfig.ts remove the welcome page

--- apps/pet-markt-web/src/app/app.ts
+++ apps/pet-markt-web/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss', 
 })
 export class App {
   protected title = 'pet-markt-web';
